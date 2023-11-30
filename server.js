948ee06b513117a007b0a38a5881ca5695c5938b
amirnz026 <czhablog@gmail.com>
2023-11-30 20:06:23 +0330

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(8080, () => {
+  console.log("JSON Server is running");
+});
