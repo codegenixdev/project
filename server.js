bea2851686cc9f43384771961d9249c345764c20
amirnz026 <czhablog@gmail.com>
2023-11-30 20:34:15 +0330

    Add delay

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const jsonServer = require("json-server");
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  setTimeout(next, 500);
+});
 server.use(router);
-server.listen(8080, () => {
-  console.log("JSON Server is running");
+
+const app = express();
+app.use(server);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Server is running on...");
 });
